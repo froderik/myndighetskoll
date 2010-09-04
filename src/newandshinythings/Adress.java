286df4a8c5c6a27d2fa1df2c19541501b3ae459a
Fredrik Rubensson <fredrik@eldfluga.se>
2010-09-04 14:06:55 +0200

    Modell...

---
+++ src/newandshinythings/Adress.java
@@ -0,0 +1,25 @@
+package newandshinythings;
+
+public class Adress {
+	private String postAdress;
+	private String postNummer;
+	private String postOrt;
+	public String getPostAdress() {
+		return postAdress;
+	}
+	public void setPostAdress(String postAdress) {
+		this.postAdress = postAdress;
+	}
+	public String getPostNummer() {
+		return postNummer;
+	}
+	public void setPostNummer(String postNummer) {
+		this.postNummer = postNummer;
+	}
+	public String getPostOrt() {
+		return postOrt;
+	}
+	public void setPostOrt(String postOrt) {
+		this.postOrt = postOrt;
+	}
+}
